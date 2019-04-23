3ae88ca76658c29374975ac97cd7b5daffff40da
jixiufeng <jixiufeng@luojilab.com>
2019-04-23 23:27:58 +0800

    add more faces,for example vterm-color-default vterm-color-black

--- elisp.h
+++ elisp.h
@@ -19,7 +19,6 @@ emacs_value Qreverse;
 emacs_value Qstrike;
 emacs_value Qface;
 emacs_value Qcursor_type;
-emacs_value Qansi_color_names_vector;
 
 // Emacs functions
 emacs_value Fsymbol_value;
@@ -43,6 +42,7 @@ emacs_value Fselected_window;
 emacs_value Fvterm_set_title;
 emacs_value Fvterm_invalidate;
 emacs_value Feq;
+emacs_value Fvterm_get_color;
 
 // Utils
 void bind_function(emacs_env *env, const char *name, emacs_value Sfun);
@@ -69,5 +69,6 @@ emacs_value get_buffer_window(emacs_env *env);
 emacs_value selected_window(emacs_env *env);
 void set_title(emacs_env *env, emacs_value string);
 void vterm_invalidate(emacs_env *env);
+emacs_value vterm_get_color(emacs_env *env, int index);
 
 #endif /* ELISP_H */
